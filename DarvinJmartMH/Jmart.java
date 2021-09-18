3a4beeda2a723da9737abb2e5b3d2af23ab92aaf
dadarvin <darvin@ui.ac.id>
2021-09-18 10:54:44 +0700

    CS Modul 2

--- DarvinJmartMH/Jmart.java
+++ DarvinJmartMH/Jmart.java
@@ -3,77 +3,125 @@ package DarvinJmartMH;
 /**
  * Write a description of class Jmart here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Darvin
+ * @version 18/09/2021
  */
+
+enum ProductCategory{
+    BOOK, KITCHEN, ELECTRONIC, FASHION, GAMING, GADGET, MOTHERCARE, FURNITURE, JEWELRY, TOYS, 
+    FNB, STATIONERY, SPORTS, AUTOMOTIVE, PETCARE, ART_CRAFT, CARPENTRY, MISCELLANEOUS, 
+    PROPERTY, TRAVEL, WEDDING;
+
+    ProductCategory(){
+    
+    }
+}
+
+class PriceTag{
+    public final static double COMMISSION_MULTIPLIER = 0.05;
+    public final static double BOTTOM_PRICE = 20000.0;
+    public final static double BOTTOM_FEE = 1000.0;
+    public double discount;
+    public double price;
+    
+    //Constructor dengan parameter price
+    public PriceTag(double price){
+        this.price = price;
+        this.discount = 0.0;
+    }
+    
+    //Constructor dengan parameter price dan discount
+    public PriceTag(double price, double discount){
+        this.price = price;
+        this.discount = discount;
+    }    
+    
+    public double getAdjustedPrice(){
+        double afterDisc = getDiscountedPrice() + getAdminFee();
+        return afterDisc;
+    }
+    
+    private double getDiscountedPrice(){
+        if(discount >= 100.0){
+            return 0.0;
+        } else{
+            return (price - (price * discount));
+        }
+    }
+    
+    public double getAdminFee(){
+        double afterDisc = getDiscountedPrice();
+        if( afterDisc <= BOTTOM_PRICE){
+            return BOTTOM_FEE;
+        } else {
+            afterDisc = afterDisc - (afterDisc * COMMISSION_MULTIPLIER);
+            return afterDisc;
+        }
+    }
+    
+}
+
+class ProductRating{
+    private long total;
+    private long count;
+    
+    public ProductRating(){
+        this.total = 0;
+        this.count = 0;
+    }
+    
+    public void insert(int rating){
+        total += rating;
+        count++;
+    }
+    
+    public double getAverage(){
+        return ((double)total/count);
+    }
+    
+    public long getCount(){
+        return count;
+    }
+    
+    public long getTotal(){
+        return total;
+    }
+}
+
+class Product{
+    private static int idCounter = 0;
+    public int id;
+    public String name;
+    public int weight;
+    public boolean conditionUsed;
+    
+    public PriceTag priceTag;
+    public ProductCategory category;
+    public ProductRating rating;
+    
+    public Product(String name, int weight, boolean conditionUsed,
+    PriceTag priceTag, ProductCategory category){
+        id = idCounter;
+        idCounter++;
+        this.name = name;
+        this.weight = weight;
+        this.conditionUsed = conditionUsed;
+        this.priceTag = priceTag;
+        this.category = category;
+        this.rating = new ProductRating();
+
+    }
+}
+
 public class Jmart
 {
     public static void main(String args[]){
-        System.out.println("Warm-Up Program - Darvin (1906300694)");
-        
-        System.out.println("Method getPromo : " + getPromo());
-        System.out.println("Method getCustomer : " + getCustomer());
-        System.out.println("Method getDiscountPercentage : " + getDiscountPercentage(1000, 0));
-        System.out.println("Method getDiscountedPrice : " + getDiscountedPrice(1000,10.0f));
-        System.out.println("Method getOriginalPrice : " + getOriginalPrice(1000, 0.0f));
-        System.out.println("Method getComissionMultiplier : " + getComissionMultiplier());
-        System.out.println("Method getAdjustedPrice : " + getAdjustedPrice(500));
-        System.out.println("Method getAdminFee : " + getAdminFee(1000));
-    }
-    
-    public static int getPromo(){
-        return 0;
-    }
-    
-    public static String getCustomer(){
-        return "oop";
-    }
-    
-    public static float getDiscountPercentage(int before, int after){
-        if(before < after){
-            return 0.0f;
-        } 
-        
-        float diff = before - after;
-        float discPercentage = diff / before * 100;
-        return (discPercentage);
         
     }
     
-    public static int getDiscountedPrice(int price, float discountPercentage){
-        float totDiscount;
-        if(discountPercentage > 100.0f){
-            totDiscount = 100.0f;
-        } else {
-            totDiscount = discountPercentage;
-        }
-        
-        float discountedPrice = price - (totDiscount * price/100);
-        int discountedPriceint = (int) discountedPrice;
-        return discountedPriceint;
-    }
-    
-    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
-        float discPriceflt = discountedPrice;
-        float originPrice = (100/(100-(discountPercentage))) * discPriceflt;
-        int originPriceint = (int) originPrice;
-        return originPriceint;
-    }
-    
-    public static float getComissionMultiplier(){
-        return 0.05f;
-    }
-    
-    public static int getAdjustedPrice(int price){
-        
-        float priceflt = price + (price * getComissionMultiplier());
-        int priceint = (int) priceflt;
-        return priceint;
-    }
-    
-    public static int getAdminFee(int price){
-        float comission = price * getComissionMultiplier();
-        int resComission = (int) comission;
-        return resComission;
+    public static Product create(){
+        PriceTag priceTag = new PriceTag(25000);
+        Product product = new Product("Fresh Cold Milk", 3, false, priceTag, ProductCategory.KITCHEN);
+        return product;
     }
 }
